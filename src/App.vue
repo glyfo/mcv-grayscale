988c2a0ae99178d8901b3da830caab6bd1606126
afuentes <alex@glyfo.com>
2023-05-18 02:46:29 +0000

    update

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,9 @@
 <template>
   <div id="app">
-    <LoadingScreen :isLoading="isLoading" />
-    <main v-if="!isLoading">
      <HomeView/>
-    </main>
   </div>
 
 </template>
 <script setup>
-import LoadingScreen from './components/LoadingScreen.vue'
 import HomeView from './views/HomeView.vue'
 </script>
-<style>
-body { margin: 0; padding: 0; background : #0A0C16 }
-</style>
---
+++ src/components/MockupIPhone.vue
@@ -0,0 +1,10 @@
+<template>
+       <div class="mockup-phone">
+       <div class="camera"></div> 
+         <div class="display">
+           <div class="artboard phone-1 artboard-demo">
+              Hi.
+           </div>
+         </div>
+    </div>
+</template>
