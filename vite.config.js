e898b938d0a9aacb277e9a429a6906643d7f3f47
afuentes <alex@glyfo.com>
2023-05-18 15:31:44 +0000

    add https support

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import mkcert from 'vite-plugin-mkcert';
 
 export default defineConfig({
-  plugins: [vue()],
+  server: { https: true },
+  plugins: [mkcert(),vue()],
 });
