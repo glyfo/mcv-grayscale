665b83abedb0fee89431c5e5e0a3667b845f16d0
afuentes <alex@glyfo.com>
2023-05-19 02:03:24 +0000

    update center desktop

--- src/views/DesktopView.vue
+++ src/views/DesktopView.vue
@@ -1,10 +1,12 @@
 <template>
+  <div class="h-screen flex flex-col  justify-center items-center">
     <div class="mockup-phone">
-    <div class="camera"></div> 
-      <div class="display">
-        <div class="artboard phone-1 artboard-demo">
-           Hi.
-        </div>
+      <div class="camera"></div> 
+         <div class="display">
+           <div class="artboard phone-1 artboard-demo">
+             Hi.
+           </div>
       </div>
- </div>
+    </div>
+</div>
 </template>
